e140306a89aa2dcaa1b54948c40f964d29f1eda0
liubeijing <1573018715@qq.com>
2017-01-06 18:50:07 +0800

    hot load

--- app.js
+++ app.js
@@ -2,93 +2,11 @@
  * Created by Administrator on 2017/1/6.
  */
 var Vue = require('vue');
-var $ = require('jquery');
+var app = require('./components/app.vue');
 
 var app = new Vue({
     el:'#pk-app',
-    data:{
-        'left': {
-            'img': '',
-            'rate': 0,
-            'dish_num': 0,
-            'id':0,
-            'dish_name':''
-        },
-        'right':{
-            'img':'',
-            'rate':0,
-            'dish_num':0,
-            'id':0,
-            'dish_name':''
-        },
-        'des':'',
-        'index':0,
-        'list':[]
-    },
-    methods:{
-        toggle:function(){
-            this.index = this.index==0?1:0;
-            this.getInfo();
-        },
-        getInfo:function(){
-            var that = this;
-            $.ajax({
-                'url':'/list.php',
-                'async':false,
-                'type':'POST',
-                'data':'index='+that.index,
-                'success':function(data){
-                    var data = $.parseJSON(data);
-                    that.list = data;
-                }
-            });
-        }
-    },
-    beforeMount:function(){
-        var that = this;
-        console.log('beforeUpdate');
-        $.ajax({
-            'url':'/pk.php',
-            'async':false,
-            'type':'POST',
-            'success':function(data){
-                var data = $.parseJSON(data);
-                console.log(data)
-                that.left = data.left;
-                that.right = data.right;
-                that.des = data.des;
-                that.index = data.index;
-                that.getInfo();
-            }
-        });
-        /*$.post('pk.php',function(data){
-         var data = $.parseJSON(data);
-         console.log(data)
-         that.left = data.left;
-         that.right = data.right;
-         that.des = data.des;
-         that.index = data.index;
-         $.post('list.php',{index:that.index},function(data){
-         var data = $.parseJSON(data);
-         console.log(data);
-         that.list = data;
-         });
-         });*/
-    },
-    mounted:function () {
-        console.log(this.$refs.list.children);
-        if(this.list.length){
-            var children = this.$refs.list.children;
-            console.log(children[0].offsetHeight);
-            for(var i=0;i<children.length;i++){
-                console.log(11);
-                children[i].style.height = 15+'rem';
-            }
-        }
-    },
-    filters:{
-        substr:function(s,from,to){
-            return s.substr(from,to);
-        }
+    components:{
+        app:app
     }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,22 @@
 var webpack = require('webpack')
 
 module.exports = {
-    entry: './app.js',
+    entry: [
+        './app.js',
+        'webpack-dev-server/client?http://127.0.0.1:9090',//资源服务器地址
+    ],
     output: {
-        path: __dirname,
+        path: __dirname+'/dist/',
+        publicPath: "http://127.0.0.1:9090/dist/",
         filename: 'bundle.js'
     },
     module: {
         loaders: [
-            {test: /\.css$/, loader: 'style!css'}
+            {test: /\.css$/, loader: 'style!css'},
+            {test:/\.vue$/,loader:'vue'}
         ]
     },
     resolve: {
-
         alias: {
             'vue$': 'vue/dist/vue.js'
         }
